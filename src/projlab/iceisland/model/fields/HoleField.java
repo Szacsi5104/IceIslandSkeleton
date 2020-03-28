1dd40c77ff9550334fd82ed63150f776eac4b525
Szacsi5104 <61117705+Szacsi5104@users.noreply.github.com>
2020-03-28 17:12:57 +0100

    Első kör Változott: Minden Field osztály

--- src/projlab/iceisland/model/fields/HoleField.java
+++ src/projlab/iceisland/model/fields/HoleField.java
@@ -3,4 +3,25 @@ package projlab.iceisland.model.fields;
 import projlab.iceisland.model.AbstractField;
 
 public class HoleField extends AbstractField {
+    @Override
+    public void step(Player player) {
+        super.step(player);
+        Underwater.interact(player);
+    }
+    @Override
+    public void snowing() {
+        if(currentState != Water || people.size() == 0){
+            super.snowing();
+        }
+    }
+    @Override
+    public int getCapacity() {
+        return 0;
+    }
+
+    @Override
+    public void update() {
+        super.update();
+        Underwater.interact(player);
+    }
 }
--- src/projlab/iceisland/model/fields/StableField.java
+++ src/projlab/iceisland/model/fields/StableField.java
@@ -3,4 +3,7 @@ package projlab.iceisland.model.fields;
 import projlab.iceisland.model.AbstractField;
 
 public class StableField extends AbstractField {
+    public int getCapacity() {
+        return Integer.MAX_VALUE;
+    }
 }
